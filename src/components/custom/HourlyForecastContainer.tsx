a7e78c1e198182ef2e21d6d05fadcd043a60f4c9
t.neroni <t.neroni@reply.it>
2026-06-25 10:40:46 +0200

    feat: remove the hourly forecast past current hour

--- src/components/custom/HourlyForecastContainer.tsx
+++ src/components/custom/HourlyForecastContainer.tsx
@@ -1,16 +1,21 @@
 import type { HourlyWeather } from "@/models/model";
 import HourlyForecast from "./HourlyForecast";
+import dayjs from "dayjs";
 
 function HourlyForecastContainer({
   hourlyForecast,
 }: {
   hourlyForecast: HourlyWeather[];
 }) {
+  const currentHour = dayjs().hour();
+
   return (
     <div className="w-full flex gap-2 overflow-x-auto mb-8">
-      {hourlyForecast.map((day) => (
-        <HourlyForecast hourlyWeatherData={day} key={day.time} />
-      ))}
+      {hourlyForecast
+        .filter((day) => dayjs(day.time).hour() >= currentHour)
+        .map((day) => (
+          <HourlyForecast hourlyWeatherData={day} key={day.time} />
+        ))}
     </div>
   );
 }
